a4dcb28cd186d472545d19cf7d5a4a4631cd9edc
leshaqwezxc <leshaqwezxc@gmail.com>
2021-03-05 15:35:08 +0300

    Initial Commit

---
+++ driveon/driveonApp.swift
@@ -0,0 +1,17 @@
+//
+//  driveonApp.swift
+//  driveon
+//
+//  Created by Алексей Тузовский on 05.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct driveonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
